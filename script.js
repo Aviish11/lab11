ff49b4f0c73a0d30943c8bfcc125903df500c13f
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-12-12 19:43:31 -0500

    fixed the getTotalValue method by updating the variables

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ class productProperties {
     }
 
     getTotalValue(price,quantity){
-        const total = price * quantity;
+        const total = this.price * this.quantity;
         return total;
     }
 
     toString(name, price, quantity){
         console.log(`Product: ${name}, Price: $ ${price}, Quantity: ${quantity}`);
-        
     }
 }
